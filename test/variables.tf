b108762b23b302c0075d78fc3cebaaf29980af07
Stanislas LESNIKOV <stl@corelia.ai>
2022-03-22 20:10:58 +0100

    Use eu-west-1 region

--- test/variables.tf
+++ test/variables.tf
@@ -1,12 +1,29 @@
-variable region {}
-variable subnet_id {}
-variable vpc_security_group_ids {}
-
-variable name {
-  default = "testing-terraform-module-selenoid"
+variable "region" {
+  default = "eu-west-1"
+}
+variable "subnet_id" {
+  default = "subnet-63a94d1a"
+}
+variable "vpc_security_group_ids" {
+  default = ["sg-03de2a80ba237ba56"]
 }
 
-variable tags {
+variable "name" {
+  default = "testing-selenoid"
+}
+
+variable "default_user" {
+  default = {
+    name                = "corelia"
+    gecos               = "Corelia Admin"
+    groups              = ["adm", "wheel"]
+    lock_passwd         = true
+    sudo                = ["ALL=(ALL) NOPASSWD:ALL"]
+    ssh_authorized_keys = ["CORELIA"] # data.github_user.me.ssh_keys
+  }
+}
+
+variable "tags" {
   default = {
     Module    = "selenoid"
     Terraform = "true",
