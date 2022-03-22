f38a205a7a3f400602a460c2ab5beed8e6cd0830
Stanislas LESNIKOV <stl@corelia.ai>
2022-03-22 20:11:39 +0100

    Remove github module

--- test/main.tf
+++ test/main.tf
@@ -2,40 +2,35 @@ terraform {
   required_version = ">= 0.12"
 
   required_providers {
-    aws    = ">= 2.50"
-    github = ">= 2.6"
+    aws = ">= 2.50"
   }
 }
 
-provider aws {
+provider "aws" {
   region = var.region
 }
 
-module test {
+module "test" {
   source = "../"
 
   name                      = var.name
-  default_user              = module.provisioner.default_user
-  users                     = [module.me.add_user]
+  default_user              = var.default_user # module.provisioner.default_user
+  users                     = []
   packages                  = ["git", "jq"]
   subnet_id                 = var.subnet_id
   vpc_security_group_ids    = var.vpc_security_group_ids
-  add_users_in_docker_group = [module.me.add_user.name]
+  add_users_in_docker_group = []
   tags                      = module.tags.tags
 }
 
-module me {
-  source = "./modules/me"
-}
-
-module provisioner {
+module "provisioner" {
   source   = "4ops/user/aws"
   version  = "1.0.0"
   key_name = var.name
   tags     = module.tags.tags
 }
 
-module tags {
+module "tags" {
   source  = "4ops/tags/null"
   version = "1.0.0"
   name    = var.name
