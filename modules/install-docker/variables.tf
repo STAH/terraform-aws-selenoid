6c2d5922aaa638247786145ea542962fb07ed501
Stanislas LESNIKOV <stl@corelia.ai>
2022-03-22 20:07:33 +0100

    Format code

--- modules/install-docker/variables.tf
+++ modules/install-docker/variables.tf
@@ -1,11 +1,11 @@
 variable "install_docker" {
   description = "?? install_docker"
-  type = bool
-  default = true
+  type        = bool
+  default     = true
 }
 
 variable "add_users_in_docker_group" {
   description = "?? add_users_in_docker_group"
-  type = list(string)
-  default = []
+  type        = list(string)
+  default     = []
 }
