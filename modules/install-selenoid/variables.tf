82d8eb9869ff5e73994fefc9df0cd04d6ec0e58b
Stanislas LESNIKOV <stl@corelia.ai>
2022-03-22 21:06:49 +0100

    Add quotes

--- modules/install-selenoid/variables.tf
+++ modules/install-selenoid/variables.tf
@@ -1,11 +1,11 @@
-variable cm_selenoid_update_ui {
+variable "cm_selenoid_update_ui" {
   default = true
 }
 
-variable cm_selenoid_update_args {
+variable "cm_selenoid_update_args" {
   default = ["--vnc"]
 }
 
-variable cm_selenoid_update_ui_args {
+variable "cm_selenoid_update_ui_args" {
   default = []
 }
